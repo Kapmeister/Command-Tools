69c650e2ebd97e1572356a808ec6140254a81c0c
Kapmeister <Kapmeister@users.noreply.github.com>
2017-08-10 17:29:16 +1000

    Added New File - ChangeIP.cmd

---
+++ ChangeIP.cmd
@@ -0,0 +1,21 @@
+@echo off
+REM
+REM This tool changes your network interface between DHCP and fixed IP addresses.
+REM Must be run as an administrator. 
+REM
+SETLOCAL ENABLEEXTENSIONS
+set FIP=192.168.0.10
+set SNM=255.255.255.0
+set DGW=192.168.0.1
+set DN1=8.8.8.8
+set DN2=8.8.4.4
+set INT="LAN"
+for /F "tokens=3" %%a in ('netsh interface ipv4 show addresses %INT% ^| find "DHCP enabled"') do set DHCP=%%a
+if "%DHCP%"=="Yes" (
+	netsh interface ip set address name=%INT% static %FIP% %SNM% %DGW%
+	netsh interface ip set dns %INT% static %DN1%
+	netsh interface ip add dns %INT% %DN2% index=2
+) else (
+	netsh interface ip set address name=%INT% dhcp
+	netsh interface ip set dns %INT% dhcp
+)
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ A set of tools run from the command prompt that perform different functions
 
 
 CleanTemp.cmd - this will delete all files and folders in your %tmp% location. 
+ChangeIP.cmd - set the variables to your fixed IP settings and this will toggle between fixed and DHCP. 
